c56b75122a2ed8aa670e6f4fec8a88b513f5ccdf
Rachele Suy <rachelesuy@gmail.com>
2019-04-09 20:12:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken? != false
     TRUE
   else
     #TRUE
